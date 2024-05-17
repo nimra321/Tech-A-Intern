a08d61325deb21ded9b6607a13d104ed3b10b624
Nimra Jahangir <nimrajahangir421@gmail.com>
2024-05-17 07:08:54 -0700

    messages

---
+++ weather-application/src/API/api.js
@@ -0,0 +1,9 @@
+const apikey = '980b9e7296a52eef29e8c9c5d120c12d';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`).then((res) => res.json()).then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
--- weather-application/src/App.js
+++ weather-application/src/App.js
@@ -1,19 +1,33 @@
 
 import './App.css';
 import {Search, MapPin, Wind} from 'react-feather';
+import getWeather from './API/api';
+import { useState } from 'react';
 
 function App() {
+
+  const [ city, setCity ] = useState("");
+  const [ weather, setWeather ] = useState({});
+
+  const getWeatherbyCity = async () => {
+    const weatherData = await getWeather(city);
+    setWeather(weatherData);
+    setCity("");
+  }
+
   return (
     <div className="app">
       <h1>Weather App</h1>
       <div className="input-wrapper">
-        <input type="text" />
-        <button>
+        <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder='Enter a City Name'/>
+        <button onClick={() => getWeatherbyCity()}>
           <Search></Search>
         </button>
       </div>
 
-      <div className="content">
+      {
+        weather && weather.weather && 
+        <div className="content">
         <div className="location d-flex">
           <MapPin></MapPin>
           <h2>Pakistan <span>(GB)</span></h2>
@@ -32,9 +46,15 @@ function App() {
 
         <div className="windstats d-flex">
           <Wind></Wind>
-          <h3>Wind is hot in pakistan &deg;</h3>
+          <h3>Wind is 20 Knots 45&deg;</h3>
         </div>
+      </div>
+      }
 
+      
+
+      <div className="content">
+        <h4>No Data Found.</h4>
       </div>
     </div>
   );
